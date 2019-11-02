ef199e315b137f2f709fbd692b448ed6780875e9
solhaga12 <44497021+solhaga12@users.noreply.github.com>
2019-11-02 13:24:54 +0100

    Update M106_M107.cpp

--- Marlin/src/gcode/plasma/M106_M107.cpp
+++ Marlin/src/gcode/plasma/M106_M107.cpp
@@ -3,19 +3,31 @@
 
 
 #include "../gcode.h"
+#include "../queue.h"
 #include "../../core/serial.h"
 #include "../../module/voltages.h"
 #include "../../module/stepper.h"
+#include "../../libs/numtostr.h"
 
 /*
  * From the profile:
  *
- *   cutterOn:  "M106",     GCode command to turn on the plasma
- *   cutterOff: "M107",     Gcode command to turn off the plasma
- *   _thcVoltage: 125,      Set the V<thcVoltage> in V. M106 parameter
- *   _delay: 100,           Set the D<pierceDelay> in ms. M106 parameter
- *   _cutHeight: 1,5,       Set the H<cutHeight> in mm. M106 parameter
- *   _initialHeight: 3,8,   Set the I<initialHeight> in mm. M106 parameter
+ *   cutterOn:  "M106"     GCode command to turn on the plasma
+ *   cutterOff: "M107"     Gcode command to turn off the plasma
+ *   _thcVoltage: 125      Set the V<thcVoltage> in V. M106 parameter
+ *   _delay: 100           Set the D<pierceDelay> in ms. M106 parameter
+ *   _cutHeight: 1,5       Set the H<cutHeight> in mm. M106 parameter
+ *   _initialHeight: 3,8   Set the I<initialHeight> in mm. M106 parameter
+ *   Use Case:
+ *   User has positioned the head.
+ *   Home Z: G28 Z
+ *   Raise to _initialHeight: G0 Z_initialHeight
+ *   Plasma Start.
+ *   Wait for Transfer
+ *   Descend to cut height: G0 Z_cutHeight
+ *   Wait Pierce Delay Time: delay(_delay)
+ *   Regulate THC: voltageManager.enableThc();
+ *
  */
 
 void GcodeSuite::M106() {
@@ -23,6 +35,7 @@ void GcodeSuite::M106() {
   uint16_t pierceDelay = 100;
   float height = 1.5;
   float initialHeight = 3.8;
+  char gcode_string[80];
 
   #define WAIT_FOR_PLASMA_LOOP 100
   #define WAIT_FOR_PLASMA 10
@@ -60,35 +73,49 @@ void GcodeSuite::M106() {
 
   SERIAL_ECHOLNPAIR("Start plasma, V = ", voltage, " D = ", pierceDelay, " H = ", height, " I = ", initialHeight);
 
-
   voltageManager.setWantedThcVoltage(voltage * SLOPE);
-  stepper.synchronize();
 
-  if (dryRun) {
-    SERIAL_ECHOLN("Start dry run");
-    TURN_PLASMA_OFF
-    voltageManager.disableThc();
-    return;
-  }
+  // Home Z and set initial height
+  //queue.inject_P(PSTR("G28 Z"));
+  sprintf_P(gcode_string, PSTR("G28 Z"));
+  process_subcommands_now(gcode_string);
+  sprintf_P(gcode_string, PSTR("G0 Z%4.3f"), initialHeight);
+  process_subcommands_now(gcode_string);
 
+
+  if (!dryRun) {
   // Start plasma torch and wait for arc transfer
   TURN_PLASMA_ON
   SERIAL_ECHOLN("PLASMA START");
 
-  for (uint8_t wait = 0; wait < WAIT_FOR_PLASMA; wait++) {
-    if (IS_PLASMA_TRANSFERRED)
-    {
-      SERIAL_ECHOLN("PLASMA TRANSFER");
-      voltageManager.enableThc();
-        return;
+    for (uint8_t wait = 0; wait < WAIT_FOR_PLASMA; wait++) {
+      if (IS_PLASMA_TRANSFERRED)
+      {
+        SERIAL_ECHOLN("PLASMA TRANSFER");
+        voltageManager.enableThc();
+        break;
+      }
+      delay(WAIT_FOR_PLASMA_LOOP);
     }
-    delay(WAIT_FOR_PLASMA_LOOP);
+    if (!IS_PLASMA_TRANSFERRED) {
+      SERIAL_ECHOLN("Plasma did not start");
+      TURN_PLASMA_OFF
+      SERIAL_ECHOLN("PLASMA STOP");
+      voltageManager.disableThc();
+      dryRun = true;
+    }
   }
-  SERIAL_ECHOLN("Plasma did not start");
-  TURN_PLASMA_OFF
-  SERIAL_ECHOLN("PLASMA STOP");
-  voltageManager.disableThc();
-  dryRun = true;
+  else {
+    SERIAL_ECHOLN("Start dry run");
+    TURN_PLASMA_OFF
+    voltageManager.disableThc();
+  }
+
+  // Decend to cut height and then delay
+  delay(pierceDelay);
+  sprintf_P(gcode_string, PSTR("G0 Z%4.3f"), height);
+  process_subcommands_now(gcode_string);
+  delay(pierceDelay);
 }
 
 void GcodeSuite::M107() {
