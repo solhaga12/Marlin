a9f5e8e8db075a38c5ccc82ef026d285ae6c765e
solhaga12 <44497021+solhaga12@users.noreply.github.com>
2021-01-31 00:10:42 +0100

    Holsd current 100% and 1/16 uSteps
    It seems like the speed can be 140 mm/s or 8250 mm/min.
    The pulley stops at the same place for F1200 and F8250.

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -2305,13 +2305,13 @@
   #endif
 
   #if COREXY_PLASMA
-    #define HOLD_MULTIPLIER    0.3  // Scales down the holding current from run current
+    #define HOLD_MULTIPLIER    1.0  // Scales down the holding current from run current
     #define INTERPOLATE       true  // Interpolate X/Y/Z_MICROSTEPS to 256
 
     #if AXIS_IS_TMC(X)
       #define X_CURRENT       2000        // (mA) RMS current. Multiply by 1.414 for peak current.
       #define X_CURRENT_HOME  800  // (mA) RMS current for sensorless homing
-      #define X_MICROSTEPS     256    // 0..256
+      #define X_MICROSTEPS     16    // 0..256
       #define X_RSENSE          0.11
       #define X_CHAIN_POS      -1    // <=0 : Not chained. 1 : MCU MOSI connected. 2 : Next in chain, ...
     #endif
@@ -2319,7 +2319,7 @@
     #if AXIS_IS_TMC(Y)
       #define Y_CURRENT       2000
       #define Y_CURRENT_HOME  800
-      #define Y_MICROSTEPS     256
+      #define Y_MICROSTEPS     16
       #define Y_RSENSE          0.11
       #define Y_CHAIN_POS      -1
     #endif
